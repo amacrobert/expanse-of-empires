2818dde4c4b352a5fde198897efced8c5f758875
Andrew MacRobert <andrew.macrobert@mequilibrium.com>
2018-05-08 16:37:18 -0400

    matchcard btns

--- assets/js/components/MatchCard.js
+++ assets/js/components/MatchCard.js
@@ -15,7 +15,10 @@ class MatchCard extends React.Component {
         var match = this.props.match;
         return(
             <div tabIndex="0" className='card match-card' onClick={this.handleMatchSelect}>
-                <h6 className={'card-header'}>{match.name}</h6>
+                <div className="card-header">
+                    <span className="align-middle">{match.name}</span>
+                    <span className="btn btn-sm btn-light btn-open">Open</span>
+                </div>
                 <div className="card-body">
                     {match.speed != 100 ? <p className="card-text">Speed: {match.speed}% </p> : null}
                     <p className="card-text">Players: 8</p>
